ba4350e4aa0cb040b2a3af9dbf7e921c4984fd19
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2024-02-20 13:19:38 +0100

    refactor: to codebuild batch (#1)

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-const core = require("@actions/core");
-const { runBuild } = require("./code-build");
-const assert = require("assert");
+const core = require('@actions/core')
+const { runBuild } = require('./code-build')
+const assert = require('assert')
 
 /* istanbul ignore next */
 if (require.main === module) {
-  run();
+  run()
 }
 
-module.exports = run;
+module.exports = run
 
 async function run() {
-  console.log("*****STARTING CODEBUILD*****");
+  console.log('*****STARTING CODEBUILD*****')
   try {
-    const build = await runBuild();
-    core.setOutput("aws-build-id", build.id);
+    const build = await runBuild()
+    core.setOutput('aws-build-id', build.id)
 
     // Signal the outcome
     assert(
-      build.buildStatus === "SUCCEEDED",
-      `Build status: ${build.buildStatus}`
-    );
+      build.buildBatchStatus === 'SUCCEEDED',
+      `Build status: ${build.buildBatchStatus}`
+    )
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   } finally {
-    console.log("*****CODEBUILD COMPLETE*****");
+    console.log('*****CODEBUILD COMPLETE*****')
   }
 }
--- local.js
+++ local.js
@@ -2,11 +2,11 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-const uuid = require("uuid/v4");
-const cp = require("child_process");
-const cb = require("./code-build");
-const assert = require("assert");
-const yargs = require("yargs");
+const uuid = require('uuid/v4')
+const cp = require('child_process')
+const cb = require('./code-build')
+const assert = require('assert')
+const yargs = require('yargs')
 
 const {
   projectName,
@@ -20,65 +20,65 @@ const {
   updateInterval,
   updateBackOff,
 } = yargs
-  .option("project-name", {
-    alias: "p",
-    describe: "AWS CodeBuild Project Name",
+  .option('project-name', {
+    alias: 'p',
+    describe: 'AWS CodeBuild Project Name',
     demandOption: true,
-    type: "string",
+    type: 'string',
   })
-  .option("buildspec-override", {
-    alias: "b",
-    describe: "Path to buildspec file",
-    type: "string",
+  .option('buildspec-override', {
+    alias: 'b',
+    describe: 'Path to buildspec file',
+    type: 'string',
   })
-  .option("compute-type-override", {
-    alias: "c",
+  .option('compute-type-override', {
+    alias: 'c',
     describe:
-      "The name of a compute type for this build that overrides the one specified in the build project.",
-    type: "string",
+      'The name of a compute type for this build that overrides the one specified in the build project.',
+    type: 'string',
   })
-  .option("environment-type-override", {
-    alias: "et",
+  .option('environment-type-override', {
+    alias: 'et',
     describe:
-      "A container type for this build that overrides the one specified in the build project.",
-    type: "string",
+      'A container type for this build that overrides the one specified in the build project.',
+    type: 'string',
   })
-  .option("image-override", {
-    alias: "i",
+  .option('image-override', {
+    alias: 'i',
     describe:
-      "The type of credentials CodeBuild uses to pull images in your build.",
-    type: "string",
+      'The type of credentials CodeBuild uses to pull images in your build.',
+    type: 'string',
   })
-  .option("image-pull-credentials-type-override", {
-    alias: "it",
+  .option('image-pull-credentials-type-override', {
+    alias: 'it',
     describe:
-      "The name of an image for this build that overrides the one specified in the build project.",
-    choices: ["CODEBUILD", "SERVICE_ROLE"],
+      'The name of an image for this build that overrides the one specified in the build project.',
+    choices: ['CODEBUILD', 'SERVICE_ROLE'],
   })
-  .option("env-vars-for-codebuild", {
-    alias: "e",
-    describe: "List of environment variables to send to CodeBuild",
-    type: "array",
+  .option('env-vars-for-codebuild', {
+    alias: 'e',
+    describe: 'List of environment variables to send to CodeBuild',
+    type: 'array',
   })
-  .option("remote", {
-    alias: "r",
-    describe: "remote name to publish to",
-    default: "origin",
-    type: "string",
+  .option('remote', {
+    alias: 'r',
+    describe: 'remote name to publish to',
+    default: 'origin',
+    type: 'string',
   })
-  .option("update-interval", {
-    describe: "Interval in seconds between API calls for fetching updates",
+  .option('update-interval', {
+    describe: 'Interval in seconds between API calls for fetching updates',
     default: 30,
-    type: "number",
+    type: 'number',
   })
-  .option("update-backoff", {
+  .option('update-backoff', {
     describe:
-      "Base update interval back-off value when encountering API rate-limiting",
+      'Base update interval back-off value when encountering API rate-limiting',
     default: 15,
-    type: "number",
-  }).argv;
+    type: 'number',
+  }).argv
 
-const BRANCH_NAME = uuid();
+const BRANCH_NAME = uuid()
 
 const params = cb.inputs2Parameters({
   projectName,
@@ -90,40 +90,40 @@ const params = cb.inputs2Parameters({
   imageOverride,
   imagePullCredentialsTypeOverride,
   envPassthrough,
-});
+})
 
 const config = {
   updateInterval: updateInterval * 1000,
   updateBackOff: updateBackOff * 1000,
-};
+}
 
-const sdk = cb.buildSdk();
+const sdk = cb.buildSdk()
 
-pushBranch(remote, BRANCH_NAME);
+pushBranch(remote, BRANCH_NAME)
 
 cb.build(sdk, params, config)
   .then(() => deleteBranch(remote, BRANCH_NAME))
   .catch((err) => {
-    deleteBranch(remote, BRANCH_NAME);
-    throw err;
-  });
+    deleteBranch(remote, BRANCH_NAME)
+    throw err
+  })
 
 function pushBranch(remote, branchName) {
-  cp.execSync(`git push ${remote} HEAD:${branchName}`);
+  cp.execSync(`git push ${remote} HEAD:${branchName}`)
 }
 
 function deleteBranch(remote, branchName) {
-  cp.execSync(`git push ${remote} :${branchName}`);
+  cp.execSync(`git push ${remote} :${branchName}`)
 }
 
 function githubInfo(remote) {
-  const gitHubSSH = "git@github.com:";
-  const gitHubHTTPS = "https://github.com/";
+  const gitHubSSH = 'git@github.com:'
+  const gitHubHTTPS = 'https://github.com/'
   /* Expecting to match something like:
    * 'fork    git@github.com:seebees/aws-codebuild-run-build.git (push)'
    * Which is the output of `git remote -v`
    */
-  const remoteMatch = new RegExp(`^${remote}.*\\(push\\)$`);
+  const remoteMatch = new RegExp(`^${remote}.*\\(push\\)$`)
   /* Not doing a grep because then I have to pass user input to the shell.
    * This way I don't have to worry about sanitizing and injection and all that jazz.
    * Further, when I _do_ pass the remote into the shell to push to it,
@@ -131,19 +131,19 @@ function githubInfo(remote) {
    * I feel confident that there are no shinanaigans.
    */
   const [gitRemote] = cp
-    .execSync("git remote -v")
+    .execSync('git remote -v')
     .toString()
-    .split("\n")
-    .filter((line) => line.trim().match(remoteMatch));
-  assert(gitRemote, `No remote found named ${remote}`);
-  const [, url] = gitRemote.split(/[\t ]/);
+    .split('\n')
+    .filter((line) => line.trim().match(remoteMatch))
+  assert(gitRemote, `No remote found named ${remote}`)
+  const [, url] = gitRemote.split(/[\t ]/)
   if (url.startsWith(gitHubHTTPS)) {
-    const [owner, repo] = url.slice(gitHubHTTPS.length, -4).split("/");
-    return { owner, repo };
+    const [owner, repo] = url.slice(gitHubHTTPS.length, -4).split('/')
+    return { owner, repo }
   } else if (url.startsWith(gitHubSSH)) {
-    const [owner, repo] = url.slice(gitHubSSH.length, -4).split("/");
-    return { owner, repo };
+    const [owner, repo] = url.slice(gitHubSSH.length, -4).split('/')
+    return { owner, repo }
   } else {
-    throw new Error(`Unsupported format: ${url}`);
+    throw new Error(`Unsupported format: ${url}`)
   }
 }
